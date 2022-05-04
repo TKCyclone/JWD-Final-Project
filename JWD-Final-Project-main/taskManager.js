ccef23b6dfc2fefb41a34af4de3f09f323b2c12f
TKCyclone <101219054+TKCyclone@users.noreply.github.com>
2022-05-04 14:45:38 -0400

    create task manager class and add method to add tasks

---
+++ JWD-Final-Project-main/taskManager.js
@@ -0,0 +1,21 @@
+//Create TaskManager
+class TaskManager {
+    constructor(currentId = 0) {
+        this.tasks = [];
+        this.currentId = currentId;
+    }
+
+    addTask(name, description, assignedTo, dueDate) {
+        const task = {
+            id: this.currentId++,
+            name: name,
+            description: description,
+            assignedTo: assignedTo,
+            dueDate: dueDate,
+            status: 'TODO'
+        };
+
+        // Push the task to the tasks property
+        this.tasks.push(task);
+    }
+}
